0e2e2cf88f80b9c45d6e3e94a58ecd38fb8a18bd
rahul.shah <rahul107.infinity@gmail.com>
2024-06-05 20:32:21 +0700

    init: First commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,34 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Education</template>
+    <a>Queen Mary University of London</a>
+    <br />
+    Masters of Law - LLM (Merit), Comparative and International Dispute Resolution, 
+    <br />
+    <br />
+    <a>National Law University, Delhi</a>
+    <br />
+    B.A. LL.B. 
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Experience</template>
+
+  </WelcomeItem>
+
+</template>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="about">
+    Under Construction
+    <div class="loader"></div>
+  </div>
+</template>
+
+<style lang="scss">
+.about {
+  font-size: 50px;
+}
+.loader {
+  border: 16px solid #f3f3f3; /* Light grey */
+  border-top: 16px solid #3498db; /* Blue */
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
